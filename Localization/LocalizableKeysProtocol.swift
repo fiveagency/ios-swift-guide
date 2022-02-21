4e58eeaae93cc5d4e1f1a173dae0d859fd36a464
Dario Nikolic <dario.nikolic@five.agency>
2022-02-21 11:20:23 +0100

    Added missing comments for Localization proposal

--- Localization/LocalizableKeysProtocol.swift
+++ Localization/LocalizableKeysProtocol.swift
@@ -1,17 +1,29 @@
+/// Formalizes accessing localization keys through `Localizable` in feature module oriented codebase.
+/// - Note: Extend `Strings` with this protocol when adding new localizable file in a feature module.
 protocol LocalizableKeysProtocol {
 
+    /// Concrete `Localizable` enumeration type used for accessing cases.
     associatedtype LocalizableKeys: Localizable
 
 }
 
+/// Provides default implementations for transforming `Localizable` case into a concrete String.
 extension LocalizableKeysProtocol {
 
+    /// Returns concrete localized String for the given localization key
+    ///
+    /// - parameter key: Localization key as enumeration case in `Localizable` enumeration
     static func localized(_ key: LocalizableKeys) -> String {
         key.localized
     }
 
-    static func localized(_ key: LocalizableKeys, arguments: [CVarArg]) -> String {
-        String(format: key.localized, arguments: arguments)
+    /// Returns concrete localized `String` for the given localization key using it as a template
+    /// into which the remaining argument values are substituted
+    ///
+    /// - parameter format: Localization key as enumeration case in `Localizable` enumeration used as a template
+    /// - parameter arguments: Values used as format substitutes
+    static func localized(format: LocalizableKeys, _ arguments: CVarArg...) -> String {
+        String(format: format.localized, arguments: arguments)
     }
 
 }
